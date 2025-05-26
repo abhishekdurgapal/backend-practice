6e061a84295b17f8ebf7e65c6a2ce522d7dc3a96
abhishekdurgapal <abhishekdurgapal367@gmail.com>
2025-05-26 20:18:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Use either local or remote MongoDB URL
-const mongoURL = process.env.MONGODB_URL_LOCAL || process.env.MONGODB_URL;
+const mongoURL = process.env.MONGODB_URL_LOCAL || process.env.MONGODB_URI;
 
 if (!mongoURL) {
   console.error('❌ MongoDB URL is not defined in environment variables.');
